e5b29e516f8cbfbfef6fe68ffc7a7fbba764ab58
Ragan McGill <ragan@themcgillfamily.co.uk>
2019-01-21 08:44:22 +0000

    tidied up the registry api

--- database_registry.api/Startup.cs
+++ database_registry.api/Startup.cs
@@ -5,12 +5,9 @@ namespace database_registry.api
 {
     public class Startup
     {
-        // This code configures Web API. The Startup class is specified as a type
-        // parameter in the WebApp.Start method.
         public void Configuration(IAppBuilder appBuilder)
         {
             HttpConfiguration config = new HttpConfiguration();
-
 
             config.MapHttpAttributeRoutes();
 
